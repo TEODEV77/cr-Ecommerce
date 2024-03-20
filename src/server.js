101fc7f8c434ee33f280c9e516b19b2b06189dae
Mateo <mateo.orolot@gmail.com>
2024-03-20 11:44:05 -0500

    Adding mongo

--- src/server.js
+++ src/server.js
@@ -3,10 +3,14 @@ import http from "http";
 import app from "./express.js";
 import { environment } from "./env.js";
 import { ServerRunning } from "./utils/messages.js";
+import MongoSingleton from "./database/mongoSingleton.js";
 
 const { port } = environment.api;
 
 const PORT = process.env.PORT || port;
+
+await MongoSingleton.getInstance();
+
 const httpServer = http.createServer(app);
 
 httpServer.listen(PORT, () => {
