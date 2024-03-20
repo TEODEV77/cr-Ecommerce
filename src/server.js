7f2214adc96254cb815afd6a74e9695982878d54
Mateo <mateo.orolot@gmail.com>
2024-03-19 20:26:54 -0500

    Deploy

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ import http from "http";
 import app from "./express.js";
 import { environment } from "./env.js";
 
-const { port, host } = environment.api;
+const { port } = environment.api;
 
 const httpServer = http.createServer(app);
 
 httpServer.listen(port, () => {
-  console.log(`Server is running at ${host}:${port}`);
+  console.log(`Server is running on$ ${port}`);
 });
