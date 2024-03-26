40e32ba7c3ee36263caf217459a360ab7c0574ed
Mateo <mateo.orolot@gmail.com>
2024-03-26 15:55:20 -0500

    Adding views, sign-up, sign-up, me

---
+++ src/routes/views/index.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+import renderRoutes from "./render.routes.js";
+import authRoutes from './auth/auth.routes.js';
+
+const router = Router();
+
+router.use("/auth", authRoutes);
+router.use("/", renderRoutes);
+
+
+export default router;
